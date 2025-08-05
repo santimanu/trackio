29d20d8470df1754f65b3a3ce7fed7e33dd81552
Santiago Rodriguez <rodriguezsanticr@gmail.com>
2025-08-05 16:59:16 -0300

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,21 +2,23 @@
 export default function Header() {
   return (
 <header className="fixed top-5 w-2/3 z-50 bg-white text-gray-900 rounded-full mt-3 mx-10">
-  <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+  <nav className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
     
     <h1 className="text-xl font-bold">Trackio AI</h1>
     
-    <nav className="space-x-4">
+    <nav className="space-x-5">
       <a href="#features" className="hover:underline">Features</a>
-      <a href="#how" className="hover:underline">Princing</a>
-      <a href="#contact" className="hover:underline">Contact</a>
+      <a href="#solutions" className="hover:underline">Solutions</a>
+      <a href="#benefits" className="hover:underline">Benefits</a>
+      <a href="#how" className="hover:underline">How it Works</a>
+      <a href="#price" className="hover:underline">Princing</a>
     </nav>
     
     <div className="space-x-4">
       <a href="">Log In</a>
       <a href="" className="">Sign Up</a>
     </div>
-  </div>
+  </nav>
 </header>
 
   );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,10 +11,38 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-  <div className="flex flex-col min-h-screen justify-center items-center">
+  <div className="flex flex-col min-h-screen items-center">
     <Header/>
 
-    <main>
+    <main className="flex justify-center bg-amber-100 h-screen w-screen flex-col text-black gap-6">
+      
+      <div className=" gap-3 flex flex-col items-center ">
+        <h1> Maximize Your Business Performance </h1>
+        <p>Let Trackio AI track, analyze, and interpret your business metrics so you can make smarter decisions—faster.</p>
+        <button className=" bg-cyan-700 rounded-xl py-2 px-4 font-medium text-white hover:scale-105 hover:bg-cyan-800 transition-all duration-200">Start Tracking for Free</button>
+        <img src="" alt="hero" />
+      </div>
+
+      <div>
+        <h3>Trusted by over 1,200 businesses across retail, SaaS, and services.</h3>
+        <p>“Trackio helped us reduce our expenses by 15% in just one quarter!” – Leo, CFO at NovaTech</p>
+      </div>
+      
+      <div>
+        <h4>Key Features</h4>
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+
     </main>
       
     <Footer/>
